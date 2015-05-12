75a6db4acd9752e4fa199619d22e99966b921717
emanon001 <emanon001@gmail.com>
2015-05-12 17:18:56 +0900

    Converter#output_http_method,output_data_format の enum を追加

--- spec/factories/converters.rb
+++ spec/factories/converters.rb
@@ -1,11 +1,10 @@
 FactoryGirl.define do
   factory :converter do
     haguruma nil
-priority 1
-input_pattern "MyText"
-output_url "MyText"
-outout_http_method 1
-output_data_format 1
+    priority 1
+    input_pattern 'MyText'
+    output_url 'MyText'
+    outout_http_method Converter.output_http_methods[:get]
+    output_data_format Converter.output_http_methods[:json]
   end
-
 end
