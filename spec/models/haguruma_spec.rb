013d49f10cf8c1a92b2bd1b3a035c99ab48add77
mollifier <mollifier@gmail.com>
2015-05-12 17:24:01 +0900

    Haguruma バリデーションを追加

--- spec/models/haguruma_spec.rb
+++ spec/models/haguruma_spec.rb
@@ -1,4 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Haguruma, type: :model do
+
+    describe "factory" do
+      it "有効なデータを作成すること" do
+        message = build(:haguruma)
+        expect(message).to be_valid
+      end
+    end
 end
