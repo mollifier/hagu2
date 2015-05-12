e162a2e170df855d0d92458258371d3386876ff2
mollifier <mollifier@gmail.com>
2015-05-12 17:57:18 +0900

    subject

--- app/models/haguruma.rb
+++ app/models/haguruma.rb
@@ -1,4 +1,5 @@
 class Haguruma < ActiveRecord::Base
   # リクエスト元から送られてくるパラメータの形式
   enum webhook_data_format: { form: 1, JSON: 2 }
+  has_many :converters, dependent: :destroy
 end
--- spec/models/haguruma_spec.rb
+++ spec/models/haguruma_spec.rb
@@ -2,10 +2,18 @@ require 'rails_helper'
 
 RSpec.describe Haguruma, type: :model do
 
-    describe "factory" do
-      it "有効なデータを作成すること" do
-        message = build(:haguruma)
-        expect(message).to be_valid
-      end
-    end
+  describe "factory" do
+    subject { build(:haguruma) }
+    it { is_expected.to be_valid }
+  end
+
+  describe "name" do
+  end
+
+  describe "display_name" do
+  end
+
+  describe "webhook_data_format" do
+  end
+
 end
